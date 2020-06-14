0109fde759c60ae80a6a3e5dc96a9bc29d48b394
Felipe Santos <felipe.santos11@hotmail.com>
2020-06-14 20:16:10 -0300

    exemplo de rota, get, get com id, post

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,19 +2,51 @@ import express from 'express';
 
 const app = express();
 
+// rota: endereço completo da requisição
+// recurso: qual entidade estamos acessando do sistema
+
+const users = [
+    'Felipe',
+    'Diego',
+    'Maik',
+    'Suzana',
+    'Janaina',
+    'Josiani'
+];
+
+//listar todos usuários
 app.get('/users', (request, response) => {
     console.log('listagem de usuários');
 
     //response.send('Olá, companheiros!');
 
-    response.json([
-        'Felipe',
-        'Diego',
-        'Maik',
-        'Suzana',
-        'Janaina',
-        'Josiani'
-    ]);
+  return response.json(users);
+});
+
+
+//listar um usuario
+app.get('/users/:id', (request, response) => {
+    
+    const id = Number (request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+})
+
+
+
+
+// criar usuario
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Felipe',
+        email: 'felipe@gmail.com',
+        idade: 33,
+        peso: 67
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);
