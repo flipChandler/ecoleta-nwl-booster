d86ba0681f8a4e6aefab454e87fdc99bf1d366f7
Felipe Santos <felipe.santos11@hotmail.com>
2020-06-15 01:17:05 -0300

    projeto limpo para iniciar o desenvolvimento do back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,62 +6,10 @@ app.use(express.json()); // add a funcionalidade express.json() no projeto
 // rota: endereço completo da requisição
 // recurso: qual entidade estamos acessando do sistema
 
-const users = [
-    'Felipe',
-    'Diego',
-    'Maik',
-    'Suzana',
-    'Janaina',
-    'Josiani'
-];
 
-//listar todos usuários
-app.get('/users', (request, response) => {
-    //http://localhost:3333/users?search=an
-    const search = String(request.query.search);
-    
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    return response.json(filteredUsers); // 
-
-  /*[
-  "Suzana",
-  "Janaina",
-  "Josiani"
-   ] */
-});
-
-
-//listar um usuario
-// request.params == @PathVariable
-// query param -> parametros opcionais dentro da rota, filtro, paginação
-// request body -> paramteros para a criação e atualização de informações
-app.get('/users/:id', (request, response) => {
-    
-    const id = Number (request.params.id);
-
-    const user = users[id];
-
-    return response.json(user);
-})
-
-
-
-
-// criar usuario
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-    console.log(data);
-
-    const user = {
-        name: data.name,
-        email: data.email,
-        idade: data.idade,
-        peso: data.peso
-    };
-
-    return response.json(user);
+app.get('/', (request, response) => {
+   return response.json( { message: 'Olá, guerreiros!'});
+  
 });
 
 app.listen(3333);
