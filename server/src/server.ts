034fdad6bf4d1add23542ca8016cd2034f5c2910
Felipe Santos <felipe.santos11@hotmail.com>
2020-06-15 01:07:07 -0300

    rota post | criar usuario

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json()); // add a funcionalidade express.json() no projeto
 // rota: endereço completo da requisição
 // recurso: qual entidade estamos acessando do sistema
 
@@ -19,9 +20,9 @@ app.get('/users', (request, response) => {
     //http://localhost:3333/users?search=an
     const search = String(request.query.search);
     
-   const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
 
-  return response.json(filteredUsers); // 
+    return response.json(filteredUsers); // 
 
   /*[
   "Suzana",
@@ -34,6 +35,7 @@ app.get('/users', (request, response) => {
 //listar um usuario
 // request.params == @PathVariable
 // query param -> parametros opcionais dentro da rota, filtro, paginação
+// request body -> paramteros para a criação e atualização de informações
 app.get('/users/:id', (request, response) => {
     
     const id = Number (request.params.id);
@@ -48,11 +50,15 @@ app.get('/users/:id', (request, response) => {
 
 // criar usuario
 app.post('/users', (request, response) => {
+    const data = request.body;
+
+    console.log(data);
+
     const user = {
-        name: 'Felipe',
-        email: 'felipe@gmail.com',
-        idade: 33,
-        peso: 67
+        name: data.name,
+        email: data.email,
+        idade: data.idade,
+        peso: data.peso
     };
 
     return response.json(user);
