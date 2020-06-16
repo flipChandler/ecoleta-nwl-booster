770e36218e1ca4445feada0f42aac781a70879e1
Felipe Santos <felipe.santos11@hotmail.com>
2020-06-16 10:21:40 -0300

    add rota estatica dos sgv's files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json()); // add a funcionalidade express.json() no projeto
 app.use(routes); // importada de routes.ts
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 
 app.listen(3333);
