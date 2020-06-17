27b05f535d9bded5d3f95a287a7a04ae15288388
Felipe Santos <felipe.santos11@hotmail.com>
2020-06-17 01:31:11 -0300

    class PointsController - refatorar create (post) await trx.commit();

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -1,6 +1,46 @@
-class PointsController{
-    async create(){
+import { Request, Response } from 'express';
+import knex from '../database/connection';
 
+class PointsController{
+    async create(request: Request, response: Response) {
+
+        //desestruturação do body em JS
+    const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
+    
+    // trx -> caso a 2ª query falhar, não execute a primeira
+    const trx = await knex.transaction();
+
+    const point = {
+        image: 'image-fake', 
+        name, // nome da variavel é o mesmo do campo da tabela
+        email, 
+        whatsapp, 
+        latitude, 
+        longitude, 
+        city, 
+        uf 
+    };
+    
+    // image not null
+    const insertedIds = await trx('points').insert(point) ;
+
+    const point_id = insertedIds[0]; // esse point_id é o msm que retorna na linha 45
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id,
+        };
+    });
+
+    await trx('points_items').insert(pointItems);
+
+    await trx.commit();
+
+    return response.json({ 
+        id: point_id,
+        ... point,
+    });
     }
 }
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import knex from './database/connection'
 
+import PointsController from './controllers/PointsController';
+
 const routes = express.Router();
+const pointsController = new PointsController();
 
 routes.use(express.json());
 
@@ -22,39 +25,7 @@ routes.get('/items', async (request, response) => {
  });
 
 
- routes.post('/points', async(request, response) => {
-    //desestruturação do body em JS
-    const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
-    
-    // trx -> caso a 2ª query falhar, não execute a primeira
-    const trx = await knex.transaction();
-    
-    // image not null
-    const insertedIds = await trx('points').insert({ 
-        image: 'image-fake', 
-        name, // nome da variavel é o mesmo do campo da tabela
-        email, 
-        whatsapp, 
-        latitude, 
-        longitude, 
-        city, 
-        uf 
-    });
-
-    const point_id = insertedIds[0]; // esse point_id é o msm que retorna na linha 45
-
-    const pointItems = items.map((item_id: number) => {
-        return {
-            item_id,
-            point_id,
-        };
-    });
-
-    await trx('points_items').insert(pointItems);
-
-    return response.json({ success: true });
-
- });
+ routes.post('/points', pointsController.create);
 
 
 
