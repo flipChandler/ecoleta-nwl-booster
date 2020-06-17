bbe9f08dff652c7df155c21bcae635dedee7def4
Felipe Santos <felipe.santos11@hotmail.com>
2020-06-16 23:37:17 -0300

    add routes (get) /items | select * from items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,10 +5,22 @@ const routes = express.Router();
 
 routes.use(express.json());
 
+
 routes.get('/items', async (request, response) => {
+    // SELECT * FROM intems;    
     const items = await knex('items').select('*');
 
-    return response.json(items);   
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+    });
+
+    return response.json(serializedItems);   
  });
+
+
+
 
  export default routes;
