5cb42123e40458c79f559152ff0a840e5c0de99a
Felipe Santos <felipe.santos11@hotmail.com>
2020-06-17 00:07:30 -0300

    routes post points - insert into points values...

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -12,12 +12,23 @@ routes.get('/items', async (request, response) => {
 
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`,
         };
     });
 
     return response.json(serializedItems);   
+ });
+
+
+ routes.post('/points', async(request, response) => {
+    //desestruturação do body em JS
+    const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
+    // image not null
+    await knex('points').insert({ image: 'image-fake', name, email, whatsapp, latitude, longitude, city, uf });
+
+    return response.json({ success: true });
  });
 
 
